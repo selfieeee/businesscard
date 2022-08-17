32249d87d36cd3e5b4e5fed5d4dff4a9a0b19c8b
Pavel_Mixajlov <1@mail.ru>
2022-08-17 17:55:02 +0600

    router start

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import './App.scss';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import {Sidebar} from "./apps/sidebar/Sidebar";
+import {Settings} from "./apps/settings/Settings";
 
 function App() {
   return (
-    <></>
+    <Router>
+      <Sidebar />
+      <Settings />
+      <Routes>
+        <Route path={""} element={<></>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
