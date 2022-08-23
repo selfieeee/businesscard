6b9f7e3d31809e7aa4f6b8afbffcea55504725ff
Pavel <ladis_76@mail.ru>
2022-08-23 22:38:00 +0600

    styled sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,47 @@
 import React from 'react';
-import './App.scss';
+import './styles.scss';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import {Sidebar} from "./apps/sidebar/Sidebar";
 import {Settings} from "./apps/settings/Settings";
+import {Grid, List, ListItem} from "@mui/material";
+import {FaHome} from 'react-icons/fa';
+import {FaUser} from 'react-icons/fa';
+import {FaList} from 'react-icons/fa';
+import {FaBriefcase} from 'react-icons/fa';
+import {FaComments} from 'react-icons/fa';
+
 
 function App() {
   return (
-    <Router>
-      <Sidebar />
-      <Settings />
-      <Routes>
-        <Route path={""} element={<></>}></Route>
-      </Routes>
-    </Router>
+    <Grid className="main-content">
+      <Grid className="aside">
+        <Grid className="logo">
+          <a href={"#"}><span>S</span>elfie</a>
+        </Grid>
+        <div className="nav-toggler">
+          <span></span>
+        </div>
+        <List className="nav">
+          <ListItem><a href={"#"} className="active"><i><FaHome className="fa fa-home"/></i> Home</a></ListItem>
+          <ListItem><a href={"#"}><i><FaUser className="fa fa-user"/></i> About</a></ListItem>
+          <ListItem><a href={"#"}><i><FaList className="fa fa-list"/></i> Services</a></ListItem>
+          <ListItem><a href={"#"}><i><FaBriefcase className="fa fa-briefcase"/></i> Portfolio</a></ListItem>
+          <ListItem><a href={"#"}><i><FaComments className="fa fa-comments"/></i> Contact</a></ListItem>
+        </List>
+      </Grid>
+      <Grid className="main-content">
+        <section className="home section">
+          home
+        </section>
+      </Grid>
+    </Grid>
+    // <Router>
+    //   <Sidebar />
+    //   <Settings />
+    //   <Routes>
+    //     <Route path={""} element={<></>}></Route>
+    //   </Routes>
+    // </Router>
   );
 }
 
