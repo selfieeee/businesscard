ab40bccd884eb81a9d7f7ffbdac98c1272012b36
Pavel <ladis_76@mail.ru>
2022-08-25 22:47:20 +0600

    fix pack

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles.scss';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,4 +10,3 @@ root.render(
   <App />
 );
 
-reportWebVitals();
