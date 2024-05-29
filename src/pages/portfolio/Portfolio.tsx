48fbc9a058b1df66fd3f961fcacbe05cfd979d90
Pavel_Mixajlov <1@mail.ru>
2024-05-29 09:41:56 +0600

    add portfolio

--- src/pages/portfolio/Portfolio.tsx
+++ src/pages/portfolio/Portfolio.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import {Grid} from "@mui/material";
+import {Link} from 'react-router-dom';
 import {Title} from "./components/Title";
 import "./Portfolio.scss";
 import {useTranslation} from "react-i18next";
+import MyGiphy from './components/MyGiphy.png';
+import CreditCard from './components/CreditCard.png';
 
 export const Portfolio = () => {
   const { t }: any = useTranslation();
@@ -15,7 +18,27 @@ export const Portfolio = () => {
             <h2>{t("Portfolio.My Last Projects")} :</h2>
           </Grid>
         </Grid>
-        {/*<Grid className="row">*/}
+        <Grid className="row">
+          <Grid className="portfolio-item padd-15">
+            <Grid className="portfolio-item-inner shadow-dark">
+              <Grid className="portfolio-img">
+                <Link
+                  to="/mygiphy">
+                  <img src={MyGiphy} alt="" />
+                </Link>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid className="portfolio-item padd-15">
+            <Grid className="portfolio-item-inner shadow-dark">
+              <Grid className="portfolio-img">
+                <Link
+                  to="/creditcard">
+                  <img src={CreditCard} alt="" />
+                </Link>
+              </Grid>
+            </Grid>
+          </Grid>
         {/*  <Grid className="portfolio-item padd-15">*/}
         {/*    <Grid className="portfolio-item-inner shadow-dark">*/}
         {/*      <Grid className="portfolio-img">*/}
@@ -44,21 +67,7 @@ export const Portfolio = () => {
         {/*      </Grid>*/}
         {/*    </Grid>*/}
         {/*  </Grid>*/}
-        {/*  <Grid className="portfolio-item padd-15">*/}
-        {/*    <Grid className="portfolio-item-inner shadow-dark">*/}
-        {/*      <Grid className="portfolio-img">*/}
-        {/*        <img src="" alt="" />*/}
-        {/*      </Grid>*/}
-        {/*    </Grid>*/}
-        {/*  </Grid>*/}
-        {/*  <Grid className="portfolio-item padd-15">*/}
-        {/*    <Grid className="portfolio-item-inner shadow-dark">*/}
-        {/*      <Grid className="portfolio-img">*/}
-        {/*        <img src="" alt="" />*/}
-        {/*      </Grid>*/}
-        {/*    </Grid>*/}
-        {/*  </Grid>*/}
-        {/*</Grid>*/}
+        </Grid>
       </Grid>
     </section>
   )
